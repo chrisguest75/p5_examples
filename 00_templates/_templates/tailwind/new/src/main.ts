486b86f4cf3398fa2dcca3bc04eac1e5881bde8b
Chris Guest <chrisguest75@users.noreply.github.com>
2025-02-22 16:30:42 +0000

    fix(template): :bug: Make sure template has all the p5 stuff ready to go.

--- 00_templates/_templates/tailwind/new/src/main.ts
+++ 00_templates/_templates/tailwind/new/src/main.ts
@@ -1,8 +1,25 @@
 ---
 to: <%= name %>/src/main.ts
 ---
-function initialise() {
-  console.log("Hello, world!"); 
-}
+import P5 from "p5";
 
-initialise();
+const sketch = (p5: P5) => {
+  p5.setup = () => {
+      p5.createCanvas(p5.windowWidth, p5.windowHeight);
+      p5.background(255);
+    };
+
+  p5.draw = () => {
+    p5.background(255);
+
+    p5.circle(p5.mouseX, p5.mouseY, 80);
+  };
+
+
+  p5.windowResized = () => {
+    p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+    p5.background(255);
+  }
+};
+
+let _instance = new P5(sketch);
