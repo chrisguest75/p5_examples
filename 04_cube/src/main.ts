37575b81b21bf10b81386e8c540e1c0dd0c440a4
Chris Guest <chrisguest75@users.noreply.github.com>
2025-02-22 22:49:27 +0000

    feat(twister): :sparkles: Split mode for rendering twister

--- 04_cube/src/main.ts
+++ 04_cube/src/main.ts
@@ -1,4 +1,5 @@
 import P5 from "p5";
+import * as d3 from 'd3';
 
 const twisterBufferHeight = 360 / 4;
 
@@ -20,6 +21,58 @@ const state: State = {
   buffer: undefined,
 };
 
+export type Mode = {
+  name: string
+  draw: (p5:P5) => void
+  default?: boolean
+}
+
+export function createTwister(p5: P5) {
+  p5.background(0);
+
+  p5.ambientLight(20);
+  p5.pointLight(
+    255, 0, 0, // color
+    40, -40, 0 // position
+  );
+  p5.directionalLight(
+    128,160,128, // color
+    1, 1, 0  // direction
+  );
+  p5.stroke(0);
+  p5.rotateY(((Math.PI * 2) / 360) * state.frameCount);
+  p5.box(400);
+
+  const line = state.frameCount % twisterBufferHeight;
+  if (state.screenCanvas) {
+    state.buffer?.copy(
+      // source
+      state.screenCanvas,
+      // source x, y, w, h
+      0, p5.windowHeight / 2, p5.windowWidth, 1,
+      // destination x, y, w, h
+      0, line, state.buffer.width, 1)
+  }
+
+
+}
+
+export function drawTwister(p5: P5) {
+  p5.background(0);
+  p5.copy(state.buffer, 0, 0, state.buffer.width, state.buffer.height, 0, 0, state.buffer.width, state.buffer.height);
+}
+
+export const modes: Mode[] = [
+  {
+      name: 'Create',
+      draw: createTwister,
+  },
+  {
+    name: 'Twist',
+    draw: drawTwister,
+  },
+]
+
 const sketch = (p5: P5) => {
   p5.setup = () => {
       const canvas = p5.createCanvas(p5.windowWidth, p5.windowHeight, p5.WEBGL);
@@ -28,38 +81,24 @@ const sketch = (p5: P5) => {
       p5.background(255);
 
       state.buffer = p5.createGraphics(600, twisterBufferHeight);
+
+      const dropdown = d3.select('#mode')
+      modes.map((background) => {
+        dropdown.append('option').attr('value', background.name).text(background.name);
+        if (background.default) {
+          dropdown.node().value = background.name;
+        }
+      });
     };
 
   p5.draw = () => {
-    p5.background(0);
-
-    p5.ambientLight(20);
-    p5.pointLight(
-      255, 0, 0, // color
-      40, -40, 0 // position
-    );
-    p5.directionalLight(
-      128,160,128, // color
-      1, 1, 0  // direction
-    );
-    p5.stroke(0);
-    p5.rotateY(((Math.PI * 2) / 360) * state.frameCount);
-    p5.box(400);
-
-    const line = state.frameCount % twisterBufferHeight;
-    if (state.screenCanvas) {
-      state.buffer?.copy(
-        // source
-        state.screenCanvas,
-        // source x, y, w, h
-        0, p5.windowHeight / 2, p5.windowWidth, 1,
-        // destination x, y, w, h
-        0, line, state.buffer.width, 1)
+    const selection = d3.select('#mode').node().value;
+    const mode = modes.find((background) => background.name === selection);
+    if (mode) {
+      mode?.draw(p5);
+    } else {
+      modes.find((mode) => mode.default)?.draw(p5);
     }
-
-    if (p5.mouseIsPressed) {
-      p5.copy(state.buffer, 0, 0, state.buffer.width, state.buffer.height, 0, 0, state.buffer.width, state.buffer.height);
-    } 
 
     state.frameCount++;
   };
